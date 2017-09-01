5208b34fb9d94524c2d75f7d1b61dfd9feff34e1
Florke <maciek.socha1@gmail.com>
2017-09-02 01:29:16 +0200

    added PlatformSprite

--- core/src/com/redartedgames/ball/myobjects/Platform.java
+++ core/src/com/redartedgames/ball/myobjects/Platform.java
@@ -4,10 +4,17 @@ import com.redartedgames.ball.objects.GameObject;
 import com.redartedgames.ball.objects.TimeObject;
 
 public class Platform extends TimeObject{
+	
+	private PlatformSprite platformSprite;
 
 	public Platform(float x, float y, int id, GameObject parent) {
 		super(x, y, id, parent);
+		platformSprite = new PlatformSprite(x, y, id, parent);
+		addSprite(platformSprite);
+		
 		// TODO Auto-generated constructor stub
 	}
+	
 
+	
 }
