4319887a6d1fcaeb4abb35ecf900f4f5ea561901
komp\Wojtek <w.mosienski@gmail.com>
2017-09-02 12:41:20 +0200

    bańka mydlana

--- core/src/com/redartedgames/ball/myobjects/Bubble.java
+++ core/src/com/redartedgames/ball/myobjects/Bubble.java
@@ -1,21 +1,41 @@
 package com.redartedgames.ball.myobjects;
 
 import com.redartedgames.ball.objects.GameObject;
-import com.redartedgames.ball.objects.Hitbox;
 import com.redartedgames.ball.objects.TimeObject;
-import com.redartedgames.ball.objects.Hitbox.BehaviorMode;
 
 public class Bubble extends TimeObject{
 
-	
+	float maxRadius;
 	float radius;
+	boolean isGrowing;
+	float growVel;
+	
+	
 	
 	public Bubble(float x, float y, int id, GameObject parent) {
 		super(x, y, id, parent);
 		radius = 0;
-		setHitbox(new Hitbox(positionX, positionY, radius, BehaviorMode.dynamic));
+		maxRadius = 150;
+		growVel = 60;
+		isGrowing = false;
 	}
 	
+	public void activate() {
+		isGrowing = true;
+	}
 	
+	public void updateLast(float delta, float vx, float vy) {
+		super.updateLast(delta, vx, vy);
+		
+		if (isGrowing) {
+			growVel = 10 + (maxRadius-radius)/3;
+			radius -= delta*growVel;
+			if (radius > maxRadius) {
+				isGrowing = false;
+				radius = maxRadius;
+			}
+		}
+		
+	}
 
 }
---
+++ core/src/com/redartedgames/ball/sprites/BubbleSprite.java
@@ -0,0 +1,17 @@
+package com.redartedgames.ball.sprites;
+
+import com.redartedgames.ball.objects.GameObject;
+import com.redartedgames.ball.objects.SpriteObject;
+
+public class BubbleSprite extends SpriteObject{
+
+	public BubbleSprite(float x, float y, GameObject parent, int id) {
+		super(x, y, parent, id);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void setRadius(float radius) {
+		
+	}
+
+}
