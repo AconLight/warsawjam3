096ea60faad090bb5cf17d1956c559b4bbdefdee
Unknown <s.robot@wp.pl>
2017-09-02 14:40:44 +0200

    ground

--- core/src/com/redartedgames/ball/myobjects/Ground.java
+++ core/src/com/redartedgames/ball/myobjects/Ground.java
@@ -1,5 +1,55 @@
 package com.redartedgames.ball.myobjects;
 
-public class Ground {
+import java.util.ArrayList;
+import java.util.Random;
 
+import com.redartedgames.ball.objects.GameObject;
+import com.redartedgames.ball.objects.TimeObject;
+
+public class Ground extends TimeObject {
+	public ArrayList<GroundSprite> plist;
+	private boolean isWater;
+	private int arrCounter = 0;
+	private float x;
+	private float y;
+	private int blockSize;
+	
+	public Ground(float x, float y, int blockSize, boolean isWater, GameObject parent, int id) {
+		super(x, y, id, parent);
+		this.x = x;
+		this.y = y;
+		this.isWater = isWater;
+		this.blockSize = blockSize;
+		plist = new ArrayList<GroundSprite>();
+		generate();
+	}
+	
+	private void generate() {
+		if(isWater) {
+			for(int i = 0; i < blockSize; i++) {
+				plist.add(new GroundSprite(x, y, 7, null, 1));
+				addSprite(plist.get(arrCounter));
+				plist.add(new GroundSprite(x, y, 8, null, 1));
+				x += plist.get(arrCounter).regionList.get(plist.get(arrCounter).regionList.size()-1).getRegionWidth();
+				arrCounter++;
+				addSprite(plist.get(arrCounter));
+			}
+		}
+		else {
+			plist.add(new GroundSprite(x, y, 1, null, 1));
+			addSprite(plist.get(arrCounter));
+			x += plist.get(arrCounter).regionList.get(plist.get(arrCounter).regionList.size()-1).getRegionWidth();
+			arrCounter++;
+			for(int i = 2; i<blockSize; i++) {
+				plist.add(new GroundSprite(x, y, new Random().nextInt(4)+2, null, 1));
+				addSprite(plist.get(arrCounter));
+				x += plist.get(arrCounter).regionList.get(plist.get(arrCounter).regionList.size()-1).getRegionWidth();
+				arrCounter++;
+			}
+			plist.add(new GroundSprite(x, y, 6, null, 1));
+			addSprite(plist.get(arrCounter));
+			x += plist.get(arrCounter).regionList.get(plist.get(arrCounter).regionList.size()-1).getRegionWidth();
+			arrCounter++;
+		}
+	}
 }
