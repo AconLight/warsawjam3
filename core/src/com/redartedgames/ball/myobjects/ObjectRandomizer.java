5c9b9eca948e789a4cba9a9c0d7c0578800c2446
Unknown <s.robot@wp.pl>
2017-09-02 23:29:09 +0200

    oko

--- core/src/com/redartedgames/ball/myobjects/ObjectRandomizer.java
+++ core/src/com/redartedgames/ball/myobjects/ObjectRandomizer.java
@@ -38,6 +38,7 @@ public class ObjectRandomizer {
 				gameObjects.add(g0);
 				player.playerSprite.collidableObjects.addAll(g0.plist);
 				player.bulletObstacles.addAll(g0.plist);
+				if(rand.nextInt(4)==0)randomClock(x + 20 + rand.nextFloat()*(g0.changeX-x), g0.getMovement().getPosition().y+270+rand.nextFloat()*30);
 				x = g0.changeX;
 				break;
 			case 1:
@@ -52,6 +53,7 @@ public class ObjectRandomizer {
 				gameObjects.add(g2);
 				player.playerSprite.collidableObjects.addAll(g2.plist);
 				player.bulletObstacles.addAll(g2.plist);
+				if(rand.nextInt(4)==0)randomObstacle(x + 20 + rand.nextFloat()*(g2.changeX-x), g2.getMovement().getPosition().y+245);
 				x = g2.changeX;
 				break;
 			case 3:
@@ -68,7 +70,7 @@ public class ObjectRandomizer {
 			default:
 				break;
 			case 0:
-				if(rand.nextInt(8) == 0) {
+				if(rand.nextInt(6) == 0) {
 					int pom = rand.nextInt(2)+6;
 					Platform p0 = new Platform(x + rand.nextFloat()*50, 300 + rand.nextFloat()*100, pom, null, 0);
 					if((p0.getMovement().getPosition().x < distanceX-50 || p0.getMovement().getPosition().x > distanceX+50*pom+50) || (p0.getMovement().getPosition().y < distanceY-50 || p0.getMovement().getPosition().y > distanceY+100)) {
@@ -80,7 +82,7 @@ public class ObjectRandomizer {
 						player.bulletObstacles.addAll(p0.plist);
 					}
 				}
-				else if(rand.nextInt(8) == 0) {
+				else if(rand.nextInt(6) == 0) {
 					int pom = rand.nextInt(1)+2;
 					Platform p0 = new Platform(x + rand.nextFloat()*50, 300 + rand.nextFloat()*100, pom, null, 0);
 					if((p0.getMovement().getPosition().x < distanceX-50 || p0.getMovement().getPosition().x > distanceX+50*pom+50) || (p0.getMovement().getPosition().y < distanceY-50 || p0.getMovement().getPosition().y > distanceY+100)) {
@@ -120,13 +122,16 @@ public class ObjectRandomizer {
 	private void randomEnemy(float x, float y, int type) {
 		Enemy e;
 		if(type == 0) e = new Enemy(x+120, y+130, 1, null, type, player);
-		else e = new Enemy(x, y, 1, null, type, player);
+		else e = new Enemy(x, y+rand.nextInt(200), 1, null, type, player);
 		gameObjects.add(e);
 		//todo dodaæ kolizje
 	}
 	
 	private void randomObstacle(float x, float y) {
-		
+		Kolce k;
+		if(rand.nextBoolean()) k = new Kolce(x, y, null, 0, 0, player);
+		else k = new Kolce(x, y-30, null, 0, 1, player);
+		gameObjects.add(k);
 	}
 	
 	private void randomClock(float x, float y) {
