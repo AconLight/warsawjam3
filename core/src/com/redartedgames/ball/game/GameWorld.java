aa25e6753d6e1a96d643741ef408a023913e00b4
Unknown <s.robot@wp.pl>
2017-09-02 10:11:20 +0200

    Auto stash before merge of "master" and "origin/master"
    oko

--- core/src/com/redartedgames/ball/game/GameWorld.java
+++ core/src/com/redartedgames/ball/game/GameWorld.java
@@ -1,23 +1,22 @@
 package com.redartedgames.ball.game;
 
 import com.redartedgames.ball.myobjects.Player;
+import com.badlogic.gdx.Gdx;
+import com.redartedgames.ball.objects.ParticleObject;
 import com.redartedgames.ball.screen.MyWorld;
 
 public class GameWorld extends MyWorld{
 	
-	Player player;
+	ParticleObject o = new ParticleObject(300, 300, 1, null);
 	
 	public GameWorld() {
-		super();
-		player = new Player(200, 200, 0, null);
-		addGameObject(player);
-				
-		
+		super();		
+		addGameObject(o);
+		o.explode(0, -10);
 	}
 	
 	@Override
 	public void update(float delta) {
 		super.update(delta);		
-	}
-	
+	}	
 }
