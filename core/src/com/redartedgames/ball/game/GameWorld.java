78a9bb4914c6b97f2789d8ff1ece7e9983decd89
komp\Wojtek <w.mosienski@gmail.com>
2017-09-02 11:34:30 +0200

    dsa

--- core/src/com/redartedgames/ball/game/GameWorld.java
+++ core/src/com/redartedgames/ball/game/GameWorld.java
@@ -1,18 +1,31 @@
 package com.redartedgames.ball.game;
 
+import java.util.ArrayList;
+
+import com.redartedgames.ball.myobjects.Platform;
 import com.redartedgames.ball.myobjects.Player;
+import com.redartedgames.ball.objects.GameObject;
 import com.redartedgames.ball.screen.MyWorld;
 
 public class GameWorld extends MyWorld{
 	
 	Player player;
+	Platform platform, p2;
+	ArrayList<GameObject> platforms;
 	
 	public GameWorld() {
 		super();
-		player = new Player(200, 200, 0, null);
+		platform = new Platform(400, 100, 600, 100, null, 0);
+		addGameObject(platform);
+		p2 = new Platform(600, 300, 100, 300, null, 0);
+		addGameObject(p2);
+		platforms = new ArrayList<GameObject>();
+		platforms.add(platform.platformSprite);
+		platforms.add(p2.platformSprite);
+		player = new Player(200, 200, 0, null, platforms);
 		addGameObject(player);
-				
 		
+		player.playerSprite.collidableObjects.add(platform.platformSprite);
 	}
 	
 	@Override
