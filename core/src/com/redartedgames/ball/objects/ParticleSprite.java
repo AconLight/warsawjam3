5bcfd3e94b426c4d26bc8b07629544b85f118ce5
Unknown <s.robot@wp.pl>
2017-09-02 10:01:44 +0200

    Particlev3

--- core/src/com/redartedgames/ball/objects/ParticleSprite.java
+++ core/src/com/redartedgames/ball/objects/ParticleSprite.java
@@ -31,7 +31,7 @@ public class ParticleSprite extends SpriteObject{
 	}
 	
 	public void updateLast(float delta, float vx, float vy) {
-		fadeTimer -= delta*2;
+		fadeTimer -= delta;
 		getMovement().setVelocity(new Vector2(getMovement().getVelocity().x*.97f,getMovement().getVelocity().y*.97f));
 	}
 	
@@ -45,7 +45,7 @@ public class ParticleSprite extends SpriteObject{
 	
 	public void render(ShapeRenderer batch, int priority, float dx, float dy) {
 		batch.setColor(rand.nextFloat(), rand.nextFloat(), rand.nextFloat(), 0);
-		batch.rect(x, y, rand.nextInt(3)+1, rand.nextInt(3)+1);
+		batch.rect(getMovement().getPosition().x, getMovement().getPosition().y, rand.nextInt(3)+1, rand.nextInt(3)+1);
 	}
 
 }
