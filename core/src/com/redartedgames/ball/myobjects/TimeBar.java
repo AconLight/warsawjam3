6a2d6168b99a956dd0fa0410261d5a0d31d27a56
Unknown <s.robot@wp.pl>
2017-09-03 02:05:02 +0200

    tajmbar

--- core/src/com/redartedgames/ball/myobjects/TimeBar.java
+++ core/src/com/redartedgames/ball/myobjects/TimeBar.java
@@ -1,9 +1,5 @@
 package com.redartedgames.ball.myobjects;
 
-import java.lang.Object;
-import java.time.Clock;
-import java.util.Random;
-
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.redartedgames.ball.objects.GameObject;
@@ -17,22 +13,18 @@ public class TimeBar extends SpriteObject{
 	public TimeBar(float x, float y, GameObject parent, int id) {
 		super(x, y, parent, id);
 		this.x = x;
-		this.y = y;
-		
+		this.y = y;		
 	}
 	
 	public void render(ShapeRenderer batch, int priority, float dx, float dy) {
-		batch.rect(x-4, y-4, 608, 28, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE);
 		batch.rect(x-2, y-2, 604, 24, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK);
-		batch.rect(x, y, timeLeft, 20, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE);
+		batch.rect(x, y, 600, 20, Color.RED, Color.GREEN, Color.GREEN, Color.RED);
+		batch.rect(x+600, y, -600+timeLeft, 20, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK);
 	}
 	
 	public void updateLast(float delta, float vx, float vy) {
-		if(timeLeft > 0) timeLeft -= delta*10;
-	}
-	
-	public void addTime(float time) {
-		if(x+time>600) x = 600;
-		else x+=time;
+		if(timeLeft > 0 && timeLeft <= 600) timeLeft -= delta*10;
+		else if(timeLeft > 600) timeLeft = 600;
+		else timeLeft = 0;
 	}
 }
