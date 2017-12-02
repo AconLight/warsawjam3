446c2496b1964797411285f97272168185d71f89
komp\Wojtek <w.mosienski@gmail.com>
2017-12-02 11:05:00 +0100

    field added

--- core/src/com/redartedgames/ball/myobjects_his/Field.java
+++ core/src/com/redartedgames/ball/myobjects_his/Field.java
@@ -1,5 +1,32 @@
 package com.redartedgames.ball.myobjects_his;
 
-public class Field {
+import java.util.ArrayList;
+
+import com.redartedgames.ball.objects.GameObject;
+import com.redartedgames.ball.objects.SpriteObject;
+
+public class Field extends SpriteObject{
+
+	public ArrayList<Field> fields;
+	
+	public Field(float x, float y, int id, GameObject parent) {
+		super(y, y, parent, id);
+		
+		//grafika Field
+	}
+	
+	public GameObject addField(Field field) {
+		fields.add(field);
+		return this;
+	}
+	
+	public void updateBefore(float delta, float vx, float vy) {
+		super.updateBefore(delta, vx, vy);
+		
+		
+		// mechanika
+	}
+	
+	
 
 }
