b4536e674edc7bbed8fc22170fae3c2d42182b94
Cyngiel <pstefanski07@gmail.com>
2017-12-02 15:35:16 +0100

    usun film test

--- core/src/com/redartedgames/ball/imgsequence/ImgSequencePlayer.java
+++ core/src/com/redartedgames/ball/imgsequence/ImgSequencePlayer.java
@@ -4,74 +4,20 @@ import java.util.ArrayList;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.redartedgames.ball.objects.GameObject;
 import com.redartedgames.ball.objects.SpriteObject;
 
-public class ImgSequencePlayer extends GameObject{
+public class ImgSequencePlayer {
 	
 	private SpriteObject seq;
-	//private ImgSequenceLoader loader;
-	private float x, y;
-	private SpriteBatch batch;
-	int n;
-	
 	ArrayList<Texture> list = new ArrayList<Texture>();
 	
-	ImgSequencePlayer(int n, float x, float y){
-		super(x,y, 1, null);
-		//this.list = list;
-		this.n = n;
-		this.x = x;
-		this.y = y;
-		
-		seq = new SpriteObject(x, y, null, 0);
-		addSprite(seq);
-		seq.setFrameTime(0.04f);
+	ImgSequencePlayer(	ArrayList<Texture> list){
+		this.list = list;
 	}
 	
 	void play(){
 		
-		seq.isStopped = false;
-		seq.isVisible = true;
-		seq.setFrameTime(0);
-		
-	}
-	
-	void stop(){
-		seq.isStopped = true;
-		seq.isVisible = false;
-	}
-	
-	void addTexture( String path){
-		System.out.println(path);
-		seq.addTexture(path);
-		
-	}
-	
-	void setPossision(float x, float y){
-		this.x = x;
-		this.y = y;
-	}
-	
-	void setVisible(boolean visible){
-		seq.isVisible = visible;
-	}
-	
-	void setPong(boolean ispong){
-		seq.setIsPingpong(ispong);
-	}
-	
-	void update(){
-		super.updateAfter(Gdx.graphics.getDeltaTime(), 0, 0);
-		
-	}
-	
-	void render(float delta){
-		batch.begin();
-		seq.updateFrames(delta);
-		seq.render(batch, 1, 0, 0);
-		batch.end();
+				
 	}
 	
 }
