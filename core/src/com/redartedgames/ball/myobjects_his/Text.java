15cf9570a25036b0a7afc8f6c4a97185f0ac1bc6
Florke07 <maciek.socha1@gmail.com>
2017-12-02 14:57:35 +0100

    poprawki w wyswietlaniu tekstóf

--- core/src/com/redartedgames/ball/myobjects_his/Text.java
+++ core/src/com/redartedgames/ball/myobjects_his/Text.java
@@ -10,10 +10,16 @@ import com.redartedgames.ball.objects.SpriteObject;
 public class Text extends SpriteObject{
 	
 	private BitmapFont font;
+	private String txt;
+	private float x_position;
+	private float y_position;
 	
 	//size 0 - small, 1 - medium, 2 - big
-	public Text(float x, float y, GameObject parent, int id, int size) { 
+	public Text(float x, float y, GameObject parent, int id, int size, String txt) { 
 		super(x, y, parent, id);
+		this.txt = txt;
+		this.x_position = x;
+		this.y_position = y;
 		switch(size) {
 		case 0:
 			font = new BitmapFont(Gdx.files.internal("data_his/fonts/font_small.fnt"),false);
@@ -29,9 +35,9 @@ public class Text extends SpriteObject{
 		}
 	}
 
-public void render(SpriteBatch batch, int priority, float dx, float yy, String str, int x_position, int y_position) {	
+public void render(SpriteBatch batch, int priority, float dx, float yy) {	
 	//font.setColor(Color.RED);
-	font.draw(batch, str, x_position, y_position);
+	font.draw(batch, txt, x_position, y_position);
 	
 	}
 }
