f9fa145e4a3d00a3230afad3fa2ad03dd50764c1
Unknown <s.robot@wp.pl>
2017-12-02 20:13:49 +0100

    Img Seq lel

--- core/src/com/redartedgames/ball/HisGame.java
+++ core/src/com/redartedgames/ball/HisGame.java
@@ -6,18 +6,22 @@ import com.badlogic.gdx.graphics.GL20;
 import com.redartedgames.ball.game_his.GameScreen_his;
 import com.redartedgames.ball.game_his.GameWorld_his;
 import com.redartedgames.ball.game_his.InputHandler_his;
+import com.redartedgames.ball.imgsequence.ImgMain;
+import com.redartedgames.ball.imgsequence.ImgSequence;
 import com.redartedgames.ball.myobjects_his.Field;
 import com.redartedgames.ball.screen.Consts;
 
 public class HisGame extends Game{
 	
 	private GameScreen_his gameScreen;
-	
+	ImgSequence seq;
 	@Override
 	public void create () {
 		gameScreen = new GameScreen_his(Consts.screenWidth, Consts.screenHeight);
 		Gdx.input.setInputProcessor(new InputHandler_his((GameWorld_his)gameScreen.getWorld()));
 		Gdx.gl.glClearColor(24f/256, 24f/256, 24f/256, 1);
+		seq = new ImgSequence(200,200,1,null);
+		seq.beginn("data_his/cards","card",4,"png");
 	}
 
 	@Override
@@ -25,6 +29,8 @@ public class HisGame extends Game{
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		gameScreen.update(Gdx.graphics.getDeltaTime());
 		gameScreen.render();
+		seq.update(Gdx.graphics.getDeltaTime());
+		seq.render();
 	}
 	
 	@Override
--- core/src/com/redartedgames/ball/imgsequence/ImgMain.java
+++
@@ -1,7 +0,0 @@
-package com.redartedgames.ball.imgsequence;
-
-import com.badlogic.gdx.ApplicationAdapter;
-
-public class ImgMain extends ApplicationAdapter{
-	ImgSequence seq = new ImgSequence("data_his/cards","card",4,"png");
-}
