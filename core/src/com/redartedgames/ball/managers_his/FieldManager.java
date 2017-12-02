0dbc65b623b850c8efb36152c23d557281a3b53a
komp\Wojtek <w.mosienski@gmail.com>
2017-12-02 12:13:54 +0100

    field manager

--- core/src/com/redartedgames/ball/managers_his/FieldManager.java
+++ core/src/com/redartedgames/ball/managers_his/FieldManager.java
@@ -2,9 +2,32 @@ package com.redartedgames.ball.managers_his;
 
 import java.util.ArrayList;
 
-import com.badlogic.gdx.utils.reflect.Field;
+import com.redartedgames.ball.myobjects_his.Field;
+
+
 
 public class FieldManager {
 	private ArrayList<Field> land;
+	
+	
+	public FieldManager() {
+		land = new ArrayList<Field>();
+		generateLand(50, 50);
+	}
+	
+	private void generateLand(int x, int y) {
+		for (int i = 0; i < y; i++) {
+			for (int j = 0; j < x; j++) {
+				land.add(new Field(j*50, i*50, 0, null));
+			}
+		}
+	}
+	
+	
+	public void update(float delta) {
+		for (Field field : land) {
+			field.updateBefore(0 ,0, delta);
+		}
+	}
 
 }
