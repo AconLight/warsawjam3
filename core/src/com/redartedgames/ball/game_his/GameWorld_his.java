284e6facd43835ac6200120381a67d5b5010a826
Unknown <s.robot@wp.pl>
2017-12-02 13:01:04 +0100

    gameworld

--- core/src/com/redartedgames/ball/game_his/GameWorld_his.java
+++ core/src/com/redartedgames/ball/game_his/GameWorld_his.java
@@ -2,15 +2,18 @@ package com.redartedgames.ball.game_his;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.redartedgames.ball.managers_his.CardsManager;
+import com.redartedgames.ball.managers_his.FieldManager;
 import com.redartedgames.ball.screen.MyWorld;
 
 public class GameWorld_his extends MyWorld{
 	
-	public CardsManager cards;
+	public CardsManager cardsManager; 
+	public FieldManager fieldManager; 
 	
 	public GameWorld_his(OrthographicCamera cam) {
 		super(cam);
-		cards = new CardsManager();
+	    cardsManager = new CardsManager(); 
+	    fieldManager = new FieldManager(); 
 	}
 	
 	public void update(float delta) {
