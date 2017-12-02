739f13573f2be2ebd54f8d4e902c03ee1dc20935
Unknown <s.robot@wp.pl>
2017-12-02 11:17:54 +0100

    mangery

---
+++ core/src/com/redartedgames/ball/managers_his/CardsManager.java
@@ -0,0 +1,8 @@
+package com.redartedgames.ball.managers_his;
+
+import java.util.ArrayList;
+import com.redartedgames.ball.myobjects_his.Card;
+
+public class CardsManager {
+	private ArrayList<Card> cards;
+}
---
+++ core/src/com/redartedgames/ball/managers_his/FieldManager.java
@@ -0,0 +1,10 @@
+package com.redartedgames.ball.managers_his;
+
+import java.util.ArrayList;
+
+import com.badlogic.gdx.utils.reflect.Field;
+
+public class FieldManager {
+	private ArrayList<Field> land;
+
+}
