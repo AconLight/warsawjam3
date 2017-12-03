0fec14c6902a23ac4f6e2465488a841efc07f41b
komp\Wojtek <w.mosienski@gmail.com>
2017-12-03 03:34:48 +0100

    efhkroivhkaagf;fhea

--- core/src/com/redartedgames/ball/game_his/renderers/StatRenderer.java
+++ core/src/com/redartedgames/ball/game_his/renderers/StatRenderer.java
@@ -19,6 +19,7 @@ public class StatRenderer extends RenderManager {
 	private String NameString="";
 	
 	private Text Money;
+	private Text AmountMoney;
 	private Text FieldName;
 	private Text StatName;
 	private Text StatBase;
@@ -30,29 +31,25 @@ public class StatRenderer extends RenderManager {
 	private Text CultureValueBase;
 	private Text Stat4Text;
 	private Text Stat4ValueBase;
-	private Text Stat5Text;
-	private Text Stat5ValueBase;
-	int i = -1;
 		
 	public StatRenderer(GameWorld_his gameworld) {
 		super(gameworld);
 		stats = new ArrayList<Text>();
 		batch = new SpriteBatch();
 		
-		Money = new Text(1580,580,null,0,2,"Pieni¹dze");
-		FieldName = new Text(1580,510,null,1,2,"NawzaPola");
-		StatName = new Text(1440,325,null,2,1,"Statystyka:");
-		StatBase = new Text(1680,325,null,3,1,"Mnoï¿½nik:");
-		MigrationText = new Text(1440,280,null,4,0,"Populacja");
-		MigrationValueBase = new Text(1685,280,null,5,0,"wartosc");
-		PrzynaleznoscText = new Text(1440,245,null,6,0,"Migracja");
-		PrzynaleznoscBase = new Text(1685,245,null,7,0,"123");
-		CultureText = new Text(1440,210,null,8,0,"PrzynaleÅ¼noÅ›Ä‡");
-		CultureValueBase = new Text(1685,210,null,9,0,"123");
-		Stat4Text = new Text(1440,175,null,10,0,"Kultura_multiper");
-		Stat4ValueBase = new Text(1685,175,null,11,0,"123");
-		Stat5Text = new Text(1440,140,null,12,0,"Stat5");
-		Stat5ValueBase = new Text(1685,140,null,13,0,"123");
+		Money = new Text(1450,510,null,0,2,"Pieni¹dze:");
+		FieldName = new Text(1575,350,null,1,2,"NawzaPola");
+		StatName = new Text(1460,260,null,2,1,"Statystyka:");
+		StatBase = new Text(1705,260,null,3,1,"Mno¿nik:");
+		MigrationText = new Text(1460,200,null,4,0,"Populacja");
+		MigrationValueBase = new Text(1705,200,null,5,0,"wartosc");
+		PrzynaleznoscText = new Text(1460,160,null,6,0,"Migracja");
+		PrzynaleznoscBase = new Text(1705,160,null,7,0,"123");
+		CultureText = new Text(1460,120,null,8,0,"Przynale¿noœæ");
+		CultureValueBase = new Text(1705,120,null,9,0,"123");
+		Stat4Text = new Text(1460,80,null,10,0,"Kultura_multiper");
+		Stat4ValueBase = new Text(1705,80,null,11,0,"123");
+		AmountMoney = new Text(1750,510,null,12,2,"123");
 		stats.add(Money);
 		stats.add(FieldName);
 		stats.add(StatName);
@@ -65,8 +62,7 @@ public class StatRenderer extends RenderManager {
 		stats.add(CultureValueBase);
 		stats.add(Stat4Text);
 		stats.add(Stat4ValueBase);
-		stats.add(Stat5Text);
-		stats.add(Stat5ValueBase);
+		stats.add(AmountMoney);
 		
 	}
 
@@ -79,31 +75,26 @@ public class StatRenderer extends RenderManager {
 	
 	public void update(float delta) {
 		super.update(delta);
-		
+		stats.get(12).txt = Integer.toString(gameworld.cardsManager.money);
 		if (gameworld.fieldManager.currentField != null) {
 			PopulacjaWartosc = Float.toString(gameworld.fieldManager.currentField.statistic.population);
 			} else PopulacjaWartosc = "";
-		stats.get(4).txt = PopulacjaWartosc;
+		stats.get(5).txt = PopulacjaWartosc;
 		
 		if (gameworld.fieldManager.currentField != null) {
 			MigracjaWartosc = Float.toString(gameworld.fieldManager.currentField.statistic.migration);
 			} else MigracjaWartosc = "";
-		stats.get(6).txt = MigracjaWartosc;
+		stats.get(7).txt = MigracjaWartosc;
 		
 		if (gameworld.fieldManager.currentField != null) {
 			PrzynaleznoscWartosc = Float.toString(gameworld.fieldManager.currentField.statistic.affiliation);
 			} else PrzynaleznoscWartosc = "";
-		stats.get(8).txt = PrzynaleznoscWartosc;
+		stats.get(9).txt = PrzynaleznoscWartosc;
 		
 		if (gameworld.fieldManager.currentField != null) {
 			KulturaWartosc = Float.toString(gameworld.fieldManager.currentField.statistic.getbase_cult_mult());
 			} else KulturaWartosc = "";
-		stats.get(10).txt = KulturaWartosc;
-		
-		if (gameworld.fieldManager.currentField != null) {
-			wartosc5 = Float.toString(gameworld.fieldManager.currentField.statistic.getbase_mig_mult());
-			} else wartosc5 = "";
-		stats.get(12).txt = wartosc5;
+		stats.get(11).txt = KulturaWartosc;
 		
 		if (gameworld.fieldManager.currentField != null) {
 			 switch(gameworld.fieldManager.currentField.fieldType){
@@ -126,8 +117,8 @@ public class StatRenderer extends RenderManager {
 			 		NameString = "";
 			 		break;
 			 }
-			stats.get(0).txt = NameString;
-		} else stats.get(0).txt = "";
+			stats.get(1).txt = NameString;
+		} else stats.get(1).txt = "";
 	}
 
 }
