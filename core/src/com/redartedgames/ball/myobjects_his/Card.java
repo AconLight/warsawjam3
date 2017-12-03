fb23049d57ffa11634525246a2ed1d3914771e13
Unknown <s.robot@wp.pl>
2017-12-03 11:50:26 +0100

    dodanie dzwieku

--- core/src/com/redartedgames/ball/myobjects_his/Card.java
+++ core/src/com/redartedgames/ball/myobjects_his/Card.java
@@ -1,5 +1,7 @@
 package com.redartedgames.ball.myobjects_his;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.math.Vector2;
 import com.redartedgames.ball.game_his.GameWorld_his;
@@ -18,6 +20,7 @@ public class Card extends GameObject{
 	public float desiredScale;
 	private float scale;
 	public int cost;
+	public Sound sound;
 	private GameWorld_his gameworld;
 	float population, migration, affliction, kurwtura;
 	
@@ -42,6 +45,7 @@ public class Card extends GameObject{
 				migration = 0;
 				affliction = 9;
 				kurwtura = 0;
+				sound = Gdx.audio.newSound(Gdx.files.internal("data_his/audio/voice/apoteoza.mp3"));
 				break;
 			case 2:
 				cost = 14;
@@ -49,6 +53,7 @@ public class Card extends GameObject{
 				migration = -5;
 				affliction = 3;
 				kurwtura = 0;
+				sound = Gdx.audio.newSound(Gdx.files.internal("data_his/audio/voice/dzieci.mp3"));
 				break;
 			case 3:
 				cost = 11;
@@ -56,6 +61,7 @@ public class Card extends GameObject{
 				migration = 0;
 				affliction = 4;
 				kurwtura = -4;
+				sound = Gdx.audio.newSound(Gdx.files.internal("data_his/audio/voice/handel.mp3"));
 				break;
 			case 4:
 				cost = 7;
@@ -63,6 +69,7 @@ public class Card extends GameObject{
 				migration = 0;
 				affliction = 0;
 				kurwtura = 0;
+				sound = Gdx.audio.newSound(Gdx.files.internal("data_his/audio/voice/higiena.mp3"));
 				break;
 			case 5:
 				cost = 17;
@@ -70,6 +77,7 @@ public class Card extends GameObject{
 				migration = -3;
 				affliction = 5;
 				kurwtura = 3;
+				sound = Gdx.audio.newSound(Gdx.files.internal("data_his/audio/voice/knajpa.mp3"));
 				break;
 			case 6:
 				cost = 16;
@@ -77,6 +85,7 @@ public class Card extends GameObject{
 				migration = 8;
 				affliction = 0;
 				kurwtura = -2;
+				sound = Gdx.audio.newSound(Gdx.files.internal("data_his/audio/voice/medycyna.mp3"));
 				break;
 			case 7:
 				cost = 9;
@@ -84,6 +93,7 @@ public class Card extends GameObject{
 				migration = 0;
 				affliction = 5;
 				kurwtura = 9;
+				sound = Gdx.audio.newSound(Gdx.files.internal("data_his/audio/voice/opera.mp3"));
 				break;
 			case 8:
 				cost = 11;
@@ -91,6 +101,7 @@ public class Card extends GameObject{
 				migration = -3;
 				affliction = 7;
 				kurwtura = -4;
+				sound = Gdx.audio.newSound(Gdx.files.internal("data_his/audio/voice/przemysl.mp3"));
 				break;
 			case 9:
 				cost = 10;
@@ -98,6 +109,7 @@ public class Card extends GameObject{
 				migration = 0;
 				affliction = 0;
 				kurwtura = -9;
+				sound = Gdx.audio.newSound(Gdx.files.internal("data_his/audio/voice/przygoda.mp3"));
 				break;
 			case 10:
 				cost = 10;
@@ -105,6 +117,7 @@ public class Card extends GameObject{
 				migration = 0;
 				affliction = 0;
 				kurwtura = 9;
+				sound = Gdx.audio.newSound(Gdx.files.internal("data_his/audio/voice/przygoda2.mp3"));
 				break;
 			case 11:
 				cost = 8;
@@ -112,6 +125,7 @@ public class Card extends GameObject{
 				migration = 0;
 				affliction = 5;
 				kurwtura = 1;
+				sound = Gdx.audio.newSound(Gdx.files.internal("data_his/audio/voice/radosc.mp3"));
 				break;
 			case 12:
 				cost = 13;
@@ -119,6 +133,7 @@ public class Card extends GameObject{
 				migration = 3;
 				affliction = 9;
 				kurwtura = 4;
+				sound = Gdx.audio.newSound(Gdx.files.internal("data_his/audio/voice/rande.mp3"));
 				break;
 			case 13:
 				cost = 10;
@@ -126,6 +141,7 @@ public class Card extends GameObject{
 				migration = -3;
 				affliction = 5;
 				kurwtura = 1;
+				sound = Gdx.audio.newSound(Gdx.files.internal("data_his/audio/voice/religia.mp3"));
 				break;
 			case 14:
 				cost = 13;
@@ -133,12 +149,14 @@ public class Card extends GameObject{
 				migration = 9;
 				affliction = 0;
 				kurwtura = 0;
+				sound = Gdx.audio.newSound(Gdx.files.internal("data_his/audio/voice/romans.mp3"));
 			case 15:
 				cost = 6;
 				population = 0;
 				migration = 0;
 				affliction = 1;
 				kurwtura = 9;
+				sound = Gdx.audio.newSound(Gdx.files.internal("data_his/audio/voice/teatr.mp3"));
 				break;
 			case 16:
 				cost = 15;
@@ -146,6 +164,7 @@ public class Card extends GameObject{
 				migration = 5;
 				affliction = 7;
 				kurwtura = 0;
+				sound = Gdx.audio.newSound(Gdx.files.internal("data_his/audio/voice/ulan.mp3"));
 				break;
 			case 17:
 				cost = 7;
@@ -153,6 +172,7 @@ public class Card extends GameObject{
 				migration = 0;
 				affliction = 7;
 				kurwtura = 1;
+				sound = Gdx.audio.newSound(Gdx.files.internal("data_his/audio/voice/widowisko.mp3"));
 				break;
 		}
 	}
@@ -167,6 +187,8 @@ public class Card extends GameObject{
 					gameworld.fieldManager.currentField.statistic.affiliation/100.0f;
 			gameworld.fieldManager.currentField.statistic.base_cult_mult += (gameworld.fieldManager.currentField.statistic.base_cult_mult*kurwtura/10)*
 					gameworld.fieldManager.currentField.statistic.affiliation/100.0f;
+			sound.play(1.0f);
+			
 		}
 		switch(id) {
 			default:
